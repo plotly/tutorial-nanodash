04ec4eb8e077f81321d8198796ddb730665ab689
Martha Cryan <martha.cryan326@gmail.com>
2024-08-15 14:21:08 -0500

    Add recursive example

--- sample.py
+++ sample.py
@@ -4,7 +4,10 @@ from nanodash.components import Component
 # Create a new Flask web server
 app = NanoDash(debug=True)
 
-simple_component = Component('button', 'Emily rocks!')
+header = Component('h1', 'Hello, world!')
+header_wrapper = Component('div', [header])
+button = Component('button', 'Click me!')
+simple_component = Component('div', [header_wrapper, button])
 app.set_layout(simple_component)
 
 app.run()

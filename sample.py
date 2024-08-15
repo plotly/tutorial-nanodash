e1c8c66abc1a5f17539db4d404142702b7e65a3b
Martha Cryan <martha.cryan326@gmail.com>
2024-08-15 14:17:48 -0500

    Add components

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ from nanodash.components import Component
 # Create a new Flask web server
 app = NanoDash(debug=True)
 
-simple_component = Component('h1', 'Emily rocks!')
+simple_component = Component('button', 'Emily rocks!')
 app.set_layout(simple_component)
 
 app.run()

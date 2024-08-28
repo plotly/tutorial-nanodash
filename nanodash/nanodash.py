bc557da43c0911d890f7e8df88823545b00a893d
Emily Kellison-Linn <4672118+emilykl@users.noreply.github.com>
2024-08-28 16:43:27 -0400

    run formatter

--- nanodash/nanodash.py
+++ nanodash/nanodash.py
@@ -1,15 +1,15 @@
 import flask
 from .components import Component
 
-class NanoDash:
 
+class NanoDash:
     def __init__(self, debug=False):
         self._app = flask.Flask(__name__)
         self._debug = debug
         self._callbacks = []
 
         # This route is used to handle callbacks
-        @self._app.route('/state', methods=['POST'])
+        @self._app.route("/state", methods=["POST"])
         def handle_change():
             print(dict(flask.request.form))
 
@@ -17,12 +17,13 @@ class NanoDash:
             # TODO: Handle prop names (right now we are assuming everything is `value`)
             for k, v in dict(flask.request.form).items():
                 for callback in self._callbacks:
-                    for input in callback['inputs']:
+                    for input in callback["inputs"]:
                         if input[0] == k:
-                            outputs = callback['function']([v])
-                            output_names = [_k for _k, _ in callback['outputs']]
+                            outputs = callback["function"]([v])
+                            output_names = [_k for _k, _ in callback["outputs"]]
                             response = {
-                                output_name: output for output_name, output in zip(output_names, outputs)
+                                output_name: output
+                                for output_name, output in zip(output_names, outputs)
                             }
                             break
 
@@ -32,10 +33,10 @@ class NanoDash:
         # Create a simple route
         # TODO: figure out how to write this so that
         # you can call set_layout multiple times without
-        # crashing the app. 
-        @self._app.route('/')
+        # crashing the app.
+        @self._app.route("/")
         def home():
-            return f'''
+            return f"""
         <html>
             <head>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -46,14 +47,12 @@ class NanoDash:
             {layout.html()}
             </body>
         </html>
-        '''
+        """
 
     def add_callback(self, inputs, outputs, function):
-        self._callbacks.append({
-            'inputs': inputs,
-            'outputs': outputs,
-            'function': function
-        })
+        self._callbacks.append(
+            {"inputs": inputs, "outputs": outputs, "function": function}
+        )
 
     def run(self):
         # Run the web server in debug mode

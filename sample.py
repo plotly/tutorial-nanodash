22e7da1f14e7468c4499f9f9a4b0101b016ba496
Martha Cryan <martha.cryan326@gmail.com>
2024-11-08 15:32:32 -0500

    Add slider example

--- sample.py
+++ sample.py
@@ -10,11 +10,20 @@ header_wrapper = Component("div", [header])
 input = Component("input", "", {"name": "input_sample"})
 output = Component("input", "", {"name": "output_sample"})
 button = Component("button", "Click me!")
+slider = Component("input", "", {"name": "slider_sample", "type": "range", "min": 0, "max": 100, "step": 1})
 graph_component = Graph(graph_obj={"data": [], "layout": {}, "config": {}}, attributes={"name": "graph-component-sample", "id": "graph-component-sample"})
-simple_component = Component("div", [header_wrapper, input, button, output, graph_component])
+simple_component = Component("div", [header_wrapper, input, button, slider, output, graph_component])
 
 app.set_layout(simple_component)
 
+def slider_callback(inputs):
+    return [inputs[0]]
+
+app.add_callback(
+    inputs=[("slider_sample", "value")],
+    outputs=[("output_sample", "value")],
+    function=slider_callback,
+)
 
 def sample_callback(inputs):
     fig = go.Figure(go.Scatter(x=[1, 2, 3], y=[1, 2, 3]))

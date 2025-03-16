7d8a6ae48a4273505b5c68987210ae6133295de6
Emily Kellison-Linn <4672118+emilykl@users.noreply.github.com>
2025-03-16 14:19:39 -0400

    set chrome options for webdriver

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -5,14 +5,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-
-
 def test_callback(sample_app, start_app):
-
     # Start the app in another thread
     start_app(sample_app)
 
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
     driver.get("http://127.0.0.1:5000")
 
     # Set text in the input with id "input_sample"
@@ -21,7 +20,9 @@ def test_callback(sample_app, start_app):
     time.sleep(2)
 
     # Check that it is showing up as the graph title
-    graph = driver.find_element(By.ID, "graph-component-sample").find_element(By.CLASS_NAME, "g-gtitle")
+    graph = driver.find_element(By.ID, "graph-component-sample").find_element(
+        By.CLASS_NAME, "g-gtitle"
+    )
     assert graph.text == "Hello, world!!"
 
     # Close the driver
